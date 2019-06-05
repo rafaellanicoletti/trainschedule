f166c7e1ef5b2342b6f4f28f60cbcaafb1d3a948
rafaella nicoletti <rafaella_np@hotmail.com>
2019-06-05 17:05:20 -0400

    variables

---
+++ app.js
@@ -0,0 +1,19 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBFUM969amA_yoxOy4wzBuoHJF3Kw4GDNs",
+    authDomain: "loginform2-6b2bb.firebaseapp.com",
+    databaseURL: "https://loginform2-6b2bb.firebaseio.com",
+    projectId: "loginform2-6b2bb",
+    storageBucket: "loginform2-6b2bb.appspot.com",
+    messagingSenderId: "351574340721",
+    appId: "1:351574340721:web:0842a607cd1fd5f3"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var trainName="";
+var destination="";
+var firstTrain=0;
+var frequency=0;
+
+
